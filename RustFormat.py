35eaa068f99efe99498df05fde1c21c2f99cb634
Ryan Davis <void@rdavis.xyz>
2016-10-01 13:40:08 +1000

    Add windows startup shell workaround

--- RustFormat.py
+++ RustFormat.py
@@ -1,10 +1,15 @@
 import sublime
 import sublime_plugin
 import subprocess
+import os
 
 
 def is_rust(view):
     return view.score_selector(0, 'source.rust') > 0
+
+
+def is_windows():
+    return os.name == 'nt'
 
 
 def settings():
@@ -16,13 +21,20 @@ class RustFormatCommand(sublime_plugin.TextCommand):
         return is_rust(self.view)
 
     def run(self, edit):
+        startupinfo = None
+        if is_windows():
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+
         binary = settings().get('rust_format_binary') or 'rustfmt'
         rustfmt = subprocess.Popen(
             [binary, '--write-mode=overwrite', self.view.file_name()],
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE,
-            shell=False,
+            startupinfo=startupinfo,
             universal_newlines=True)
+
         output, error = rustfmt.communicate()
         if error:
             print('RustFormat:', error)
